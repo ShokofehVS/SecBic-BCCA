474c2955fa94c43b6f06492f90cdc40f1d4967ef
NK <n_k>
2023-05-30 17:14:48 +0200

    load and preprocess yeast data set

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy as np
+from data_processing import load_yeast_tavazoie
+
+# load yeast data used in the original Cheng and Church's paper
+data = load_yeast_tavazoie().values
+
+# missing value imputation suggested by Cheng and Church
+missing = np.where(data < 0.0)
+data[missing] = np.random.randint(low=0, high=800, size=len(missing[0]))
+
+
+
+print(data)
+print(data.shape)
